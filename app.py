af5730b8eae1cb414a2e96bffca6cc43b3201883
taki-21 <taki.it.0208.21@gmail.com>
2022-07-17 23:34:48 +0900

    update: handler

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import sys
+
+
 def handler(event, context):
+    print('event: ', event)
+    print('type_of_event: ', type(event))
     return 'Hello from AWS Lambda using Python' + sys.version + '!'
